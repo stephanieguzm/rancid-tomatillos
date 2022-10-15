7258128afd58f646fd9dd846c0aa4d9d1e59a1f6
Stephanie Guzman <sguzman247@gmail.com>
2022-10-15 13:39:53 -0500

    Write fetch calls for API data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import MoviesContainer from '../MoviesContainer/MoviesContainer'
-import movieData from '../../movieData'
 import Navbar from '../Navbar/Navbar'
 import IndividualMovie from '../IndividualMovie/IndividualMovie'
 import './App.css'
@@ -8,22 +7,53 @@ import './App.css'
 class App extends Component {
   constructor() {
     super()
-    this.state = { movies: movieData.movies, selectedMovie: "" }
+    this.state = { 
+      movies: [], 
+      selectedMovie: null,
+      error: '',
+      isLoading: true
+     }
+  }
+
+  componentDidMount() {
+    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+      .then( response=> {
+        if (!response.ok) {
+          throw new Error()
+        }
+        return response.json() 
+      })
+      .then( data => this.setState({ movies: data.movies, loading: false}))
+      .catch( error => this.setState({ error: error.message }))
   }
 
   handleClick = (id) => {
-  const userSelectedMovie = this.state.movies.find(movie => movie.id === id)
-  this.setState({selectedMovie: userSelectedMovie})
+    const userSelectedMovie = this.state.movies.find(movie => movie.id === id)
+    fetch(`https://rancid-tomatillos.herokuapp.com/api/v2//movies/${userSelectedMovie.id}`)
+      .then( response=> {
+        if (!response.ok) {
+          throw new Error()
+        }
+        return response.json() 
+      })
+      .then( data => this.setState({ selectedMovie: data.movie }))
+      .catch( error => this.setState({ error: error.message }))
   }
 
   returnHome = () => {
-    this.setState({ selectedMovie: "" })
+    this.setState({ selectedMovie: null })
   }
 
   render() {
     return (
       <div>
         <Navbar selectedMovie={this.state.selectedMovie} returnHome={this.returnHome}/>
+        {this.state.isLoading && 
+          <h2>Page is Loading...</h2>
+          // <img />
+        }
+        {this.state.error && 
+          <h2 className='error-text'>There was an error: {this.state.error}. Movies cannot load. Please try again.</h2>}
         {!this.state.selectedMovie && <MoviesContainer movies={this.state.movies} handleClick={this.handleClick}/>}
         {this.state.selectedMovie && 
         <IndividualMovie selectedMovie={this.state.selectedMovie}/>
