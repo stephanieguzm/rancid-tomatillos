228542925e86c77fe203727b5be9627614c4c2ce
Morgan Berry <morganlyn.berry@gmail.com>
2022-10-15 09:16:16 -0600

    Add userSelected variable

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,28 +9,20 @@ import Navbar from '../Navbar/Navbar'
 class App extends Component {
   constructor() {
     super()
-    this.state = { movies: movieData.movies}
+    this.state = { movies: movieData.movies, selectedMovie: {}}
   }
 
   handleClick = (id) => {
         console.log('id!', id)
-        // this.findSelectedMovie(event)
-    //this method will call on a method within the App class and pass through the movie selection (event.target.value)
-  }
-
-  findSelectedMovie = () => {
-    console.log('Hello from App!')
-    // use find() to locate the movie with the id that the user selected
-    // id is passed as a parameter
-    // then we'll find the right movie by matching the movie.id to the user selected movie id
+  const userSelectedMovie = this.state.movies.find(movie => movie.id === id)
+  this.setState({selectedMovie: userSelectedMovie})
   }
 
   render() {
     return (
       <div>
-        {/* <h1 className='header'>Rancid Tomatillos</h1> */}
         <Navbar />
-        <MoviesContainer movies={this.state.movies}/>
+        <MoviesContainer movies={this.state.movies} handleClick={this.handleClick}/>
       </div>
     )
   }
