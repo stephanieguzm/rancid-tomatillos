4c6df66cf0edde892430dd187e5d73791a5db8e0
Stephanie Guzman <sguzman247@gmail.com>
2022-10-24 09:53:32 -0500

    Modify Route paths to route users for error handling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import MoviesContainer from '../MoviesContainer/MoviesContainer'
 import Navbar from '../Navbar/Navbar'
 import IndividualMovie from '../IndividualMovie/IndividualMovie'
-import './App.css'
 import Error from '../Error/Error'
+import './App.css'
 
 class App extends Component {
   constructor() {
@@ -33,24 +33,25 @@ class App extends Component {
   }
 
   render() {
-    console.log('hasError', this.state.hasError)
+    console.log(this.state.hasError)
     return (
       <div>
         <Navbar />
-       
         <Route exact path='/'
-          render={() => <MoviesContainer movies={this.state.movies} /> } />
-       <Route exact 
+          render={() => <MoviesContainer 
+          movies={this.state.movies} 
+          error={this.state.hasError}
+          /> 
+        } />
+        <Route exact 
           path='/:id'
           render={({ match }) => {
             const matchId = match.params.id
             return <IndividualMovie selectedMovie={matchId} />
           }
-        }/>
-        {this.state.hasError && <Route exact path='*'
-       render={()=> <Error />} />}
-       {!this.state.movies.length && <p className="spinner"></p>}
-     
+        } />
+        {/* {this.state.hasError && <Error />}  */}
+        {!this.state.movies.length && !this.state.hasError ? <p className="spinner"></p> : <></>}
       </div>
     )
   }
