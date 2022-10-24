17b332f0abac1a3a43347d275fbc1d0d5e16753b
Morgan Berry <morganlyn.berry@gmail.com>
2022-10-24 09:20:19 -0600

    Add error tests to individual movie_spec

--- cypress/e2e/individual_movie_spec.cy.js
+++ cypress/e2e/individual_movie_spec.cy.js
@@ -7,10 +7,31 @@ describe('As a user, when I click on a movie, I am shown additional details abou
     cy.visit('http://localhost:3000/').wait(2000)
   })
 
-  it('should display an error message (500 status code) if the individual movie is unable to render', () => {
+  it('should display an error message (500 status code) if a movies details are unable to be displayed on the screen', () => {
+    cy.intercept(
+      "GET",
+      "https://rancid-tomatillos.herokuapp.com/api/v2/movies/694919",
+      {
+        statusCode: 500,
+        body: {
+          error: "Not Found",
+        },
+      })
+      cy.visit("http://localhost:3000/694919")
+      cy.get(".error-text").contains(`You didn't break the internet, but we can't find what you are looking for... Please visit our page later.`);
+  });
+
+  it('should display a page loading icon while waiting for a movie to display on page', () => {
     cy
-      .intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/581392', 
-      {statusCode: 500, body: {message: `Movies cannot load. Please try again.` }})
+    .get('.movie-card').first().click()
+    .intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/694919', {
+      fixture: 'movie1.json'
+    }).as('movie')
+      .visit('http://localhost:3000/694919')
+      .get('.spinner').should('exist')
+      .wait('@movie').then(() => {
+        cy.get('.spinner').should('not.exist')
+    })
   })
 
   it('should render all the details about the movie they clicked on', () => {
@@ -29,7 +50,7 @@ describe('As a user, when I click on a movie, I am shown additional details abou
   })
 
 
-  it.skip('should render all the details about a different movie they clicked on', () => {
+  it('should render all the details about a different movie they clicked on', () => {
     cy
       .get('.movie-card').last().click()
       .intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/581392', {
@@ -46,7 +67,7 @@ describe('As a user, when I click on a movie, I am shown additional details abou
 
 
 
-  it.skip('should no longer display the dashboard that displays all the movies', () => {
+  it('should no longer display the dashboard that displays all the movies', () => {
     cy
       .visit('http://localhost:3000/694919').wait(2000)
       .url().should('eq', 'http://localhost:3000/694919')
@@ -54,16 +75,16 @@ describe('As a user, when I click on a movie, I am shown additional details abou
       .get('.movie-container').should('not.exist')
   })
   
-  it.skip('should display the navbar with the application logo and home button when I am on an individual movie view', () => {
+  it('should display the navbar with the application logo and home button when I am on an individual movie view', () => {
     cy
       .visit('http://localhost:3000/694919').wait(2000)
       .url().should('eq', 'http://localhost:3000/694919')
       .get('.navBar').should('exist')
       .get('.logo').should('exist') 
-      .get('.home-button').contains('home')
+      .get('.home-button').contains('Go Home')
   })
 
-  it.skip('should take a user back home when the home button or Rancid Tomatillo logo is clicked', () => {
+  it('should take a user back home when the home button or Rancid Tomatillo logo is clicked', () => {
     cy
     .visit('http://localhost:3000/694919').wait(2000)
     .url().should('eq', 'http://localhost:3000/694919')
