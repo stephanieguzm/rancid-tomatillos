83394b64ee43f1c4431f8a97663494e336bfbc36
Morgan Berry <morganlyn.berry@gmail.com>
2022-10-24 09:58:09 -0600

    Fix small syntax errors

--- cypress/e2e/movies_container_spec.cy.js
+++ cypress/e2e/movies_container_spec.cy.js
@@ -17,9 +17,10 @@ describe('As a user, when I load the application, I can see a collection of movi
         body: {
           error: "Not Found",
         },
-      })
-      cy.visit("http://localhost:3000/")
-      cy.get(".error-text").contains(`You didn't break the internet, but we can't find what you are looking for... Please visit our page later.`);
+      }
+    )
+    cy.visit("http://localhost:3000/")
+    cy.get(".error-text").contains(`You didn't break the internet, but we can't find what you are looking for... Please visit our page later.`);
   });
 
   it('should display the navbar with the application logo and home button', () => {
@@ -66,8 +67,7 @@ describe('As a user, when I load the application, I can see a collection of movi
       .url().should('eq', 'http://localhost:3000/')
       .go('forward')
       .url().should('eq', 'http://localhost:3000/694919')
-  })
-  
+  }) 
 })
 
 
